15f0bc8ef5ac11f82efaf83ff3b812fab9bb8db1
maggiolil <luca.maggioli@nagra.com>
2021-11-29 12:04:00 +0100

    changing auth_config json into a ts file

---
+++ auth_config.ts
@@ -0,0 +1,13 @@
+// {
+//   "domain": "dev-yp49lez.eu.auth0.com",
+//   "clientId": "vhDmHs9CRD8VGQa1ejPeBpZHFr2Q9lEg",
+//   "audience": "https://express.sample",
+//   "serverUrl": "http://localhost:6060"
+// }
+
+export const domain = 'dev-yp49lez.eu.auth0.com';
+export const clientId = 'vhDmHs9CRD8VGQa1ejPeBpZHFr2Q9lEg';
+export const audience = 'https://express.sample';
+export const serverUrl = 'http://localhost:6060';
+
+
