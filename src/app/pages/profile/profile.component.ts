f46ac3c11a23d1917401be2bf448ce8f62762d47
maggiolil <luca.maggioli@nagra.com>
2021-11-29 10:40:39 +0100

    add: get info from user when it log

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  profileJson: string = null;
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe(
+      // (profile) => (this.profileJson = JSON.stringify(profile, null, 2)),
+      (profile) => {
+        this.profileJson = JSON.stringify(profile, null, 2);
+        console.log('profile and profileJson');
+        console.log(profile);
+        console.log(this.profileJson);
+      }
+    );
+  }
 }
